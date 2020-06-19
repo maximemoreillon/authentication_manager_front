c1b29e619e65111606af7e2f92459263c1cde46f
Maxime Moreillon <m.moreillon@gmail.com>
2020-06-19 12:22:53 +0000

    now allowing email address as user identifier

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="login_view">
 
+    <!-- login form -->
     <form
       v-if="!logged_in"
       v-on:submit.prevent="login()">
-      <input type="text" v-model="input_data.username" placeholder="username">
-      <input type="password" v-model="input_data.password" placeholder="password">
+
+      <div class="">
+        <label>Username / email</label>
+        <input type="text" v-model="input_data.identifier">
+      </div>
+
+      <div class="">
+        <label>Password</label>
+        <input type="password" v-model="input_data.password">
+      </div>
+
       <input type="submit" v-bind:value="login_button_text">
-      <div class="error_message">{{error_message}}</div>
+
+      <span class="error_message">{{error_message}}</span>
 
     </form>
 
+    <!-- logout form -->
     <form
       v-else
       v-on:submit.prevent="logout()">
       <div class="">Logged in as {{username}}</div>
       <!-- submit input had a bug not showing the value -->
-      <button type="button" v-on:click="logout()">Logout</button>
+      <input type="submit" value="logout">
     </form>
 
   </div>
@@ -34,7 +46,7 @@ export default {
     return{
 
       input_data: {
-        username: "",
+        identifier: "",
         password: "",
       },
 
@@ -54,13 +66,13 @@ export default {
       this.logging_in = true;
       this.error_message = "";
 
-      this.axios.post(process.env.VUE_APP_AUTHENTICATION_API_URL + "/login", {
-        username: this.input_data.username,
+      this.axios.post(`${process.env.VUE_APP_AUTHENTICATION_API_URL}/login`, {
+        identifier: this.input_data.identifier,
         password: this.input_data.password
       })
       .then(response => {
 
-        this.logging_in = false;
+
         this.error_message = null;
 
         // save the cookie
@@ -74,6 +86,7 @@ export default {
 
       })
       .catch(error => { this.error_message = error.response.data })
+      .finally(() => {this.logging_in = false})
     },
     logout(){
       //this.$cookies.remove('jwt', null, process.env.VUE_APP_COOKIE_DOMAIN)
@@ -111,26 +124,44 @@ export default {
 </script>
 
 <style scoped>
+.login_view {
+
+}
 
 form {
   display: flex;
   flex-direction: column;
   align-items: center;
+  text-align: center;
+  font-size: 120%;
 }
 
 form > * {
-  margin: 10px;
-  padding: 10px;
-  width: 250px;
-  text-align: center;
+  margin: 1em 0;
 }
+
+form > div {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+form label {
+  color: #666666;
+  font-size: 75%;
+}
+
+
 
 .error_message {
   color: #c00000;
 }
 
-.button_icon{
-  cursor: pointer;
+input, button {
+  width: 50vw;
+  max-width: 500px;
+  font-size: 100%;
+  padding: 0.25em;
 }
 
 input[type="text"],input[type="password"] {
